d7542c0bcdaf2655c645a8c34840d5e3cb8a7947
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-17 14:28:36 +0530

    [samarjeet]: added find age code

---
+++ gcr-codebase/programming-elements/level-1-practice-program/FindAge.java
@@ -0,0 +1,8 @@
+class FindAge{
+public static void main(String[]args){
+// find the age of harry in the year 2024
+int birthYear=2000, currentYear=2024;
+
+System.out.println("harry's age in 2024 is:"+ (currentYear-birthYear));
+}
+}

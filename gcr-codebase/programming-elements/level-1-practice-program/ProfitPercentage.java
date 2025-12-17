f75ca135d0957864f452f98a49894c274dbac385
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-17 15:08:42 +0530

    [samarjeet]: added program of profit percentage

---
+++ gcr-codebase/programming-elements/level-1-practice-program/ProfitPercentage.java
@@ -0,0 +1,7 @@
+public class ProfitPercentage{
+public static void main(String[]args){
+double costPrice=129 , sellingPrice=191;
+double profit=sellingPrice-costPrice , profitPercentage = (profit/costPrice)*100;
+System.out.println("The Cost Price is INR" + costPrice + "and Selling Price is INR" + sellingPrice + "\n" +"The Profit is INR" + profit +" and the Profit Percentage is " + profitPercentage);
+}
+}

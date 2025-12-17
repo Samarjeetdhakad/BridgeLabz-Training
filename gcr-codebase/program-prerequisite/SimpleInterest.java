41d71365d5bfb5d1540759ac316a41377d888c37
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-17 12:30:25 +0530

    [samarjeet]:added addition of two number code

---
+++ gcr-codebase/program-prerequisite/SimpleInterest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+ class SimpleInterest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double principal = sc.nextDouble();
+
+        double rate = sc.nextDouble();
+
+        double time = sc.nextDouble();
+
+        double simpleInterest = (principal * rate * time) / 100;
+
+        System.out.println(simpleInterest);
+    }
+}

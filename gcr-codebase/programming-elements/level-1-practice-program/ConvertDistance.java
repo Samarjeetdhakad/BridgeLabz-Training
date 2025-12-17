d544b5c93841dba7e7e30689295a36533456233a
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-17 14:50:48 +0530

    [samarjeet]: added distance convert code

---
+++ gcr-codebase/programming-elements/level-1-practice-program/ConvertDistance.java
@@ -0,0 +1,7 @@
+public class ConvertDistance{
+public static void main(String[]args){
+double Kilometers=10.8;
+double Miles = Kilometers*1.6;
+System.out.println(" The distance " + Kilometers + " km in miles is" + Miles);
+}
+}

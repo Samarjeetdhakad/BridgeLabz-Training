97e32d772af35e35972179cd88e9249f9018607e
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-17 14:36:53 +0530

    [samarjeet]: added average percent marks code

---
+++ gcr-codebase/programming-elements/level-1-practice-program/AveragePercentMark.java
@@ -0,0 +1,7 @@
+public class AveragePercentMark{
+public static void main(String[]args){
+ int Math=94, Physics=95, Chemistry=96;
+ int average=(Math+Physics+Chemistry)/3;
+ System.out.println("average percent mark is :" + average);
+}
+}

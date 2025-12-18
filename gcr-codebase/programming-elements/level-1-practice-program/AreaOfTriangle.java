16876c455a916fa6796eb108ccc80da0a2c34694
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-18 11:45:24 +0530

    [samarjeet]: added the program of finding area og triangle in sqcm and sqinches

--- gcr-codebase/programming-elements/level-1-practice-program/AreaOfTriangle.java
+++ gcr-codebase/programming-elements/level-1-practice-program/AreaOfTriangle.java
@@ -1,29 +1,18 @@
-import java.util.Scanner;
-
-public class TriangleArea {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-       
-        double base = sc.nextDouble();
-        double height = sc.nextDouble();
-
-        // Area in square centimeters
-        double areaSqCm = 0.5 * base * height;
-
-        
-        double baseInInches = base / 2.54;
-        double heightInInches = height / 2.54;
-
-        // take the area in square inches
-        double areaSqIn = 0.5 * baseInInches * heightInInches;
-
-        
-        System.out.println(
-            "The Area of the triangle in sq in is " + areaSqIn +
-            " and sq cm is " + areaSqCm);
-
-        
-    }
+//finding area of traingle by taking user input
+import java.util.*;
+public class AreaOfTriangle{
+	public static void main(String[]args){
+		
+		//take the input from the user in inches
+		double height , base;
+		Scanner input = new Scanner (System.in);
+		base = input.nextDouble();
+		height = input.nextDouble();
+		double areaInInches = 0.5 * base * height;
+		double areaInCm = 0.5 * ( base*2.54 )* (height * 2.54);
+		
+		//showing the area of tringle in cm and inches
+		System.out.println("Your area in sqcm is " + areaInCm + " area in sqinches is " + areaInInches );
+		
+	}
 }

9b65d8da8125140abb72de320e672d6801f68569
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-18 14:35:44 +0530

    [samarjeet]: added a program to show the countdown for rocket

---
+++ gcr-codebase/control-flows/level-1-program-practice/CountDown.java
@@ -0,0 +1,14 @@
+//write a program to show the countdown of rocket launch from the user input to 1
+import java.util.*;
+public class CountDown{
+	public static void main(String[]args){
+		
+		//take input value of the countdown starting from the user 
+		Scanner sc= new Scanner(System.in);
+		int counter = sc.nextInt();
+		while(counter>=1){
+			System.out.println(counter);
+			counter--;
+		}
+	}
+}

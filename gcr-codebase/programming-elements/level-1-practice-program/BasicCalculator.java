aa5b5a2179999f97f952908ac65daea8e9674618
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-18 08:00:40 +0530

    [samarjeet]: added logic of creating a basic calculator for addition, substraction, multiplication and division

---
+++ gcr-codebase/programming-elements/level-1-practice-program/BasicCalculator.java
@@ -0,0 +1,28 @@
+//program to perform basic mathematical ooperations 
+import java.util.*;
+public class BasicCalculator {
+	public static void main (String[]args){
+		
+		//declairing and taking input of two numbers
+		double number1, number2;
+		Scanner input =  new Scanner (System.in);
+		number1 = input.nextDouble();
+		number2 = input.nextDouble();
+		double addition = number1 + number2;
+		double substraction = number1-number2;
+		double multiplication  = number1 * number2;
+		double division   = number1 / number2;
+		
+		
+		//printing the calculated values
+		System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " + number1+ "and "+ number2 +  " is" + addition + ", " + substraction + ", " + multiplication + " and " + division );
+
+		
+		
+
+		
+		
+		
+	}
+
+}

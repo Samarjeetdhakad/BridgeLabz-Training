dca58c1b0ceb31de46efc6bac9b228ddbdf1e245
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-22 11:35:45 +0530

    [samarjeet]:added a program of finding the smallest and largest number

---
+++ gcr-codebase/methods/level-1-practice-program/SmallestLargest.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SmallestLargest{
+	
+	public static void 
+	
+	public static void main(String[]args){
+		
+		Scanner sc =new Scanner(System.in);
+		int number1=sc.next();
+		int number2=sc.next();
+		int number3=sc.next();
+		
+		findSmallestAndLargest(int number1, int number2, int number3);
+
+	}
+}

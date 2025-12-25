77991c48fcbee7c2f202038057d078c5796ae0f0
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-25 16:40:09 +0530

    [samarjeet]:added a program of showing date in different format '

---
+++ gcr-codebase/extras/buil-in-function-problem-practice/DateFormatting.java
@@ -0,0 +1,22 @@
+// write a program to show date in different form
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class DateFormatting{
+	public static void main(String[]args){
+		
+		LocalDate date = LocalDate.now();
+		
+		DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		DateTimeFormatter format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		
+		DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+		
+		System.out.println(date.format(format1));
+		System.out.println(date.format(format2));
+		System.out.println(date.format(format3));
+		
+		
+		
+	}
+}

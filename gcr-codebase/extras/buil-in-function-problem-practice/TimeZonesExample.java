e31d52cac28efca7b9f03c456b0c742d0583068b
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-25 14:01:53 +0530

    [samarjeet]:added a program of showing the different zone time

---
+++ gcr-codebase/extras/buil-in-function-problem-practice/TimeZonesExample.java
@@ -0,0 +1,19 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class TimeZonesExample {
+    public static void main(String[] args) {
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss z ");
+		//GMT 
+		ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+		System.out.println("current timein GTM" + gmtTime.format(formatter));
+        // IST (Indian Standard Time)
+        ZonedDateTime istTime=ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Time in IST: "+istTime.format(formatter));
+        // PST (Pacific Standard Time)
+        ZonedDateTime pstTime=ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println("Current Time in PST: "+pstTime.format(formatter));
+    }
+}

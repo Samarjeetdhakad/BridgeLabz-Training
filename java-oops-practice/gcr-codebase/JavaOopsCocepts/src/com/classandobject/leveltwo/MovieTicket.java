fa83fc321727c698d11377f1389923c0e03ca55e
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-27 11:00:14 +0530

    [samarjeet]: modified the known error

--- java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/classandobject/leveltwo/MovieTicket.java
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/classandobject/leveltwo/MovieTicket.java
@@ -6,20 +6,18 @@ public class MovieTicket {
 	private double price;
 	private boolean isBooked;
 	
-	//Creating the constructor
 	MovieTicket(String movieName){
 		this.movieName=movieName;
 		this.seatNumber = "Null";
 		this.price = 0.0 ;
 		this.isBooked = false;
 	}
-	//Creating the method to book ticket
 	public void bookTicket(String seatNumber,double price) {
-		//Checking weather the ticket is booked or not
+
 		if(isBooked) {
 			System.err.println("House full!!! sorry..... Ticket already booked");
 		}
-		//getting the details if not booked
+
 		else {
 			this.seatNumber = seatNumber;
 			this.price = price;
@@ -29,15 +27,14 @@ public class MovieTicket {
            System.out.println("Price: $" + this.price);
 		}
 	}
-	//Crating the method to display the booked tickets
+
 	public void displayTicket() {
-		//checking whether the ticket is already booked
+
 		if(this.isBooked) {
 			System.out.println("Ticket booked for movie: " + this.movieName);
            System.out.println("Seat Number: " + this.seatNumber);
            System.out.println("Price: $" + this.price);	
 		}
-		//displaying message If failed to book the ticket
 		else {
 			System.out.println("Ticket have not booked yet....");
 		}
@@ -50,7 +47,7 @@ public class MovieTicket {
 		ticket1.displayTicket();
 		ticket1.bookTicket("A10",120.0);
 		ticket1.bookTicket("B10", 120.0);
-		ticket1.bookTicket("A11", 120.0);//booking the ticket which is already booked
+		ticket1.bookTicket("A11", 120.0);
 		ticket1.displayTicket();
 	}
 }

d43f20eb29ff4a2795159d05279e53184f930658
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2025-12-30 11:35:30 +0530

    [samarjeet]: added main method of library program

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/thisstatic/Library.java
@@ -0,0 +1,13 @@
+package com.thisstatic;
+
+public class Library {
+	   public static void main(String[] args) {
+
+	       Book.setLibraryName("Egmore Library");
+	       Book.displayLibraryName();
+
+	       Book book1 = new Book("Effective Java", "Joshua Bloch", "978-0134685991");
+
+	       book1.displayBookDetails();
+	   }
+	}

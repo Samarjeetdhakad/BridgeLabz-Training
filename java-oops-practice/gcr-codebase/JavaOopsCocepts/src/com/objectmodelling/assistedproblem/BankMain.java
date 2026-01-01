89cea7b60cb5b16a40fc55345380ab53b66ec368
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-01 16:49:54 +0530

    [samarjeet]: added a program of object modelling (bank and account holder name)

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/objectmodelling/assistedproblem/BankMain.java
@@ -0,0 +1,20 @@
+package com.objectmodelling.assistedproblem;
+
+public class BankMain {
+    public static void main(String[] args) {
+
+        Bank bank = new Bank("State Bank", 5);
+
+        Customer c1=new Customer("Aman",3);
+        Customer c2=new Customer("Riya",3);
+
+        bank.openAccount(c1,101,5000);
+        bank.openAccount(c1,102,12000);
+        bank.openAccount(c2,201,8000);
+
+        System.out.println();
+        c1.viewBalance();
+        System.out.println();
+        c2.viewBalance();
+    }
+}

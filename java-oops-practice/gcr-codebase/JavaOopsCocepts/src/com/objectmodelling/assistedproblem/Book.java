5b71fc7d61de4c062b740ba6c2cca12ba08cd830
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-01 16:26:14 +0530

    [samarjeet] : Added first program of object modelling (book and library )

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/objectmodelling/assistedproblem/Book.java
@@ -0,0 +1,15 @@
+package com.objectmodelling.assistedproblem;
+
+class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    void showBook() {
+        System.out.println("Title: " + title + ", Author: " + author);
+    }
+}

f4ffc94d941cc5307c18c40534d95d59d7fab194
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-02 17:04:15 +0530

    [ samarjeet ] : added library management with books and author

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/inheritance/singleinheritance/BookMain.java
@@ -0,0 +1,13 @@
+package com.inheritance.singleinheritance;
+
+public class BookMain {
+
+	public static void main(String[] args) {
+		
+		Book b = new Author("Java Programming", 2024, "Rohan Sharma","Java developer and technical writer");
+
+        b.displayInfo();
+
+	}
+
+}

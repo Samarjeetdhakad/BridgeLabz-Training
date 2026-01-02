94c64f4a798444b40531589fcc101f310d66ba07
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-02 17:23:20 +0530

    [samarjeet] : added online retail management

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/inheritance/multilevelinheritance/onlineretailordermanagement/Order.java
@@ -0,0 +1,16 @@
+package com.inheritance.multilevelinheritance.onlineretailordermanagement;
+
+
+public class Order {
+	 int orderId;
+	    String orderDate;
+
+	    Order(int orderId, String orderDate) {
+	        this.orderId = orderId;
+	        this.orderDate = orderDate;
+	    }
+
+	    String getOrderStatus() {
+	        return "Order Placed";
+	    }
+}

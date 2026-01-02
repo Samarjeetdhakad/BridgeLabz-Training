661adddbf83ee53a0cfc87f8b469400ecbe7c40b
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-02 16:42:01 +0530

    [samarjeet] : added Employee management system

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/inheritance/assistedproblems/Intern.java
@@ -0,0 +1,13 @@
+package com.inheritance.assistedproblems;
+
+public class Intern extends Employee {
+	
+	Intern(String name, int id, int salary) {
+        super(name, id, salary);
+       
+    }
+	
+	void displayDetails(){
+		super.displayDetails();
+	}
+}

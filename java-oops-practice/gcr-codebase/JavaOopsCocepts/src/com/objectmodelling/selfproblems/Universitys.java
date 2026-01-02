e9e2d47ce192bac26afe0529b3033c0d6e73a4db
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-02 12:46:20 +0530

    [samarjeet]:added university management

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/objectmodelling/selfproblems/Universitys.java
@@ -0,0 +1,22 @@
+package com.objectmodelling.selfproblems;
+
+class Universitys {
+    String name;
+    Students[] students;
+    Professor[] professors;
+    int studentCount = 0, professorCount = 0;
+
+    Universitys(String name, int maxStudents, int maxProfessors) {
+        this.name = name;
+        students = new Students[maxStudents];
+        professors = new Professor[maxProfessors];
+    }
+
+    void addStudent(Students student) {
+        students[studentCount++] = student;
+    }
+
+    void addProfessor(Professor professor) {
+        professors[professorCount++] = professor;
+    }
+}

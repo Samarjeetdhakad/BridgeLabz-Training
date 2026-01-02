b4a6418fde51c9dbbf657cea0a38620e4d5894ff
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-02 16:40:58 +0530

    [samarjeet] : added Employee management system

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/inheritance/assistedproblems/Developer.java
@@ -0,0 +1,17 @@
+package com.inheritance.assistedproblems;
+
+
+public class Developer extends Employee {
+	String programmingLanguage ;
+
+	Developer(String name, int id, int salary,String lang) {
+        super(name, id, salary);
+        this.programmingLanguage = lang;
+    }
+	
+	void displayDetails(){
+		super.displayDetails();
+		System.out.println("Programming Language: " + programmingLanguage);
+	}
+
+}

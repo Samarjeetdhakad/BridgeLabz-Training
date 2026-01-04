db29675fe34d7f495461428345aaa11c2a9cce00
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-04 23:51:44 +0530

    [samarjeet]: added a program of employee management system

---
+++ java-oops-practice/gcr-codebase/JavaOopsCocepts/src/com/encapsulationpolymorphisminterfaceandabstract/employeemanagementsystem/Department.java
@@ -0,0 +1,6 @@
+package com.encapsulationpolymorphisminterfaceandabstract.employeemanagementsystem;
+
+interface Department {
+    void assignDepartment(String deptName);
+    String getDepartmentDetails();
+}

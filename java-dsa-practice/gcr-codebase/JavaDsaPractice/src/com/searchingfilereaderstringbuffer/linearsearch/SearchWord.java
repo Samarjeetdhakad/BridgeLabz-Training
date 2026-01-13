aadffb984df90eae06c63683dd56c08f1f69f249
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-13 11:41:38 +0530

    [samarjeet]: added a program of finding word in sentences in linear search

---
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/searchingfilereaderstringbuffer/linearsearch/SearchWord.java
@@ -0,0 +1,31 @@
+package com.searchingfilereaderstringbuffer.linearsearch;
+
+import java.util.*;
+public class SearchWord {
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+	    sc.nextLine();
+		String s[]=new String[n];
+
+		for(int i=0;i<n;i++) {
+			s[i]=sc.nextLine();
+		}
+		String word=sc.nextLine();
+		String index=" ";
+		for(int i=0;i<s.length;i++) {
+			if(s[i].contains(word)) {
+				index=s[i];
+				break;
+		}
+		}
+		if(index !=" ") {
+			System.out.println(index);
+		}
+		else {
+			System.out.println("not found");
+		}
+		
+	}
+
+}

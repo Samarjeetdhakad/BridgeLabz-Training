ff7d6bd6c215ee0226be018882fe443c242be821
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-15 16:16:15 +0530

    [samarjeet] : modified program

--- java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/searchingfilereaderstringbuffer/binarysearch/FirstAndLastOccurance.java
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/searchingfilereaderstringbuffer/binarysearch/FirstAndLastOccurance.java
@@ -42,7 +42,7 @@ public class FirstAndLastOccurance {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 Scanner sc=new Scanner(System.in);
 System.out.println("enter input");
 int n=sc.nextInt();

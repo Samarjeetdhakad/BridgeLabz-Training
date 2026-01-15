8740b8351b7158e4a986ce9b7a8f230a482cdc5d
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-15 13:01:56 +0530

    [samarjeet]: added filereader program read file

---
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/searchingfilereaderstringbuffer/filereader/CountOccurance.java
@@ -0,0 +1,28 @@
+package com.searchingfilereaderstringbuffer.filereader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CountOccurance {
+	public static void main(String[] args) {
+
+	        File file=new File("sample.txt"); 
+	        String targetWord="java";         
+	        int count=0;
+	        try (Scanner sc=new Scanner(file)){
+	            while(sc.hasNext()){
+
+	                String word=sc.next().toLowerCase(); 
+	                word=word.replaceAll("\\W+", "");
+	                if(word.equals(targetWord.toLowerCase())){
+	                    count++;
+	             }
+	            }
+	            System.out.println("The word \"" + targetWord + "\" appears " + count + " times in the file.");
+	        }
+	        catch (FileNotFoundException e) {
+	            System.out.println("File not found: " + e.getMessage());
+	    }
+	    }
+	    }
---
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/searchingfilereaderstringbuffer/filereader/ReadLine.java
@@ -0,0 +1,19 @@
+package com.searchingfilereaderstringbuffer.filereader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class ReadLine{
+	public static void main(String[] args) {
+        File file=new File("sampl.txt"); 
+        try(Scanner sc=new Scanner(file)){
+            while(sc.hasNextLine()){
+                String line=sc.nextLine();
+                System.out.println(line);
+         }
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+  }
+ }
+}

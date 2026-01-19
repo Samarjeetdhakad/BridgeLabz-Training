0303a5ac9714e986abb28d1099d979bb97aef97f
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-19 12:25:27 +0530

    [ samarjeet]: added a parcel tracker scenario based program

---
+++ java-dsa-practice/scenario-based/DsaScenarioPractice/src/parceltracker/Main.java
@@ -0,0 +1,16 @@
+package parceltracker;
+public class Main {
+
+    public static void main(String[] args) {
+
+        ParcelTracker tracker=new ParcelTracker();
+        tracker.addInitialStage("Packed");
+        tracker.addStage("Shipped");
+        tracker.addStage("In Transit");
+        tracker.addStage("Delivered");
+
+        tracker.addCheckpointAfter("In Transit", "Reached City Hub");
+
+        tracker.track();
+    }
+}

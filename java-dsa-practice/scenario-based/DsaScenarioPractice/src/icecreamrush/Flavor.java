440219868dab8c26a085d4a1058b7ceafd884c37
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-19 17:17:57 +0530

    [ samarjeet]: added ice cream rush a scenario based programming

---
+++ java-dsa-practice/scenario-based/DsaScenarioPractice/src/icecreamrush/Flavor.java
@@ -0,0 +1,16 @@
+package icecreamrush;
+
+public class Flavor {
+    String name;
+    int sales;
+
+    public Flavor(String name, int sales) {
+        this.name = name;
+        this.sales = sales;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s |Sales: %d", name, sales);
+    }
+}

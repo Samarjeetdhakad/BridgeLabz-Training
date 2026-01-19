384114d27d8480a1b5c6742e0f74ada84ca86f9b
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-19 16:28:43 +0530

    [ samarjeet]:added zip zip mart a scenario based program

---
+++ java-dsa-practice/scenario-based/DsaScenarioPractice/src/zipzipmart/Transaction.java
@@ -0,0 +1,10 @@
+package zipzipmart;
+public class Transaction {
+    String date;   // YYYY-MM-DD
+    double amount;
+
+    public Transaction(String date, double amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+}

10a9339f5a714bee60b90208aa8dba941225bd62
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-19 11:55:22 +0530

    [ samarjeet]: added a smartcehckout customer scenario based program

---
+++ java-dsa-practice/scenario-based/DsaScenarioPractice/src/smartcheckout/Customer.java
@@ -0,0 +1,13 @@
+package smartcheckout;
+
+import java.util.*;
+class Customer {
+    String customerName;
+    ArrayList<Item>cart=new ArrayList<>();
+    Customer(String customerName) {
+        this.customerName=customerName;
+ }
+    public void addItem(String item, int qty) {
+        cart.add(new Item(item, qty));
+}
+}
---
+++ java-dsa-practice/scenario-based/DsaScenarioPractice/src/smartcheckout/Item.java
@@ -0,0 +1,11 @@
+package smartcheckout;
+
+
+public class Item {
+String name;
+int qty;
+Item(String name,int qty){
+	this.name=name;
+	this.qty=qty;
+}
+}

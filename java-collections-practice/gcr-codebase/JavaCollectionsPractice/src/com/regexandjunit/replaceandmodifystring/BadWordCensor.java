f2451ff5b5d10cf0008313a29f00537e9c04c27c
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-27 00:16:22 +0530

    [samarrjeet]:added bad word censor program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/regexandjunit/replaceandmodifystring/BadWordCensor.java
@@ -0,0 +1,10 @@
+package com.regexandjunit.replaceandmodifystring;
+
+public class BadWordCensor {
+    public static void main(String[] args) {
+        String text = "This is a damn bad example with some stupid words.";
+        String regex = "\\b(damn|stupid)\\b";
+        String result = text.replaceAll(regex, "****");
+        System.out.println(result);
+    }
+}

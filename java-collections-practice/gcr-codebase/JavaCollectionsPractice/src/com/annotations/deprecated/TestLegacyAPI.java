9eadeef5bfe68caccde4f00a299f4d8f9bec2471
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-28 00:36:11 +0530

    [samarjeet]:added test legacy api

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/deprecated/TestLegacyAPI.java
@@ -0,0 +1,9 @@
+package com.annotations.deprecated;
+
+public class TestLegacyAPI {
+    public static void main(String[] args) {
+        LegacyAPI api = new LegacyAPI();
+        api.oldFeature();   
+        api.newFeature();   
+    }
+}

9723efad3f759c3821067b3592643ff7311b30d0
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-28 00:42:24 +0530

    [samarjeet]:create a custom annotation and use it

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/custom/TaskManager.java
@@ -0,0 +1,8 @@
+package com.annotations.custom;
+
+public class TaskManager {
+    @TaskInfo(priority = 1, assignedTo = "Sunny")
+    public void completeTask() {
+        System.out.println("Task completed");
+    }
+}

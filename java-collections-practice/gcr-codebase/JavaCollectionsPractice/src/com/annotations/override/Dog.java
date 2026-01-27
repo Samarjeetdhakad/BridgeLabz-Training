b3af6af86914f5ccf4d8005f27d6725021ef2d9b
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-28 00:33:21 +0530

    [samarjeet]:added overrride annotation

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/override/Dog.java
@@ -0,0 +1,8 @@
+package com.annotations.override;
+
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/override/TestAnimal.java
@@ -0,0 +1,8 @@
+package com.annotations.override;
+
+public class TestAnimal {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}

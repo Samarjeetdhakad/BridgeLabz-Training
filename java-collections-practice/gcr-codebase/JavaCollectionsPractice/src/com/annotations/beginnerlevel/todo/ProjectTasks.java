de37552b15feda5fbcb370d0cc8d62159980a537
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-28 11:30:50 +0530

    [samarjeet]: Create a @todo Annotation for Pending Tasks

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/beginnerlevel/todo/ProjectTasks.java
@@ -0,0 +1,25 @@
+package com.annotations.beginnerlevel.todo;
+
+public class ProjectTasks {
+    @Todo(
+        task = "Implement login feature",
+        assignedTo = "samarjeet",
+        priority = "HIGH"
+    )
+    public void loginFeature() {
+    }
+    @Todo(
+        task = "Add forgot password functionality",
+        assignedTo = "Amit"
+    )
+    public void forgotPassword() {
+    }
+
+    @Todo(
+        task = "Improve UI design",
+        assignedTo = "Neha",
+        priority = "LOW"
+    )
+    public void improveUI() {
+    }
+}

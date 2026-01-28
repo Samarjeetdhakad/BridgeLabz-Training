b756e67d45c1b003e4b9260c0a3eb1251027457f
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-28 12:30:15 +0530

    [samarjeet]: added: access private field

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/basiclevel/accessprivatefield/AccessPrivateField.java
@@ -0,0 +1,27 @@
+package com.reflection.basiclevel.accessprivatefield;
+import java.lang.reflect.Field;
+
+public class AccessPrivateField {
+    public static void main(String[] args) {
+        try {
+            // Create object
+            Person person = new Person(25);
+            // Get Class object
+            Class<?> cls = person.getClass();
+            // Access private field "age"
+            Field ageField = cls.getDeclaredField("age");
+
+            // Make private field accessible
+            ageField.setAccessible(true);
+            int ageValue = (int) ageField.get(person);
+            System.out.println("Original Age: " + ageValue);
+
+            ageField.set(person, 30);
+            //Get updated value
+            int updatedAge = (int) ageField.get(person);
+            System.out.println("Updated Age: " + updatedAge);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/basiclevel/accessprivatefield/Person.java
@@ -0,0 +1,8 @@
+package com.reflection.basiclevel.accessprivatefield;
+
+public class Person {
+    private int age;
+    public Person(int age) {
+        this.age = age;
+    }
+}

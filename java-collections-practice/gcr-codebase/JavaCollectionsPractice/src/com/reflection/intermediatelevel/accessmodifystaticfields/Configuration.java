027854fdd752e5d846265a0af0db99e6ee8f0634
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-28 12:44:55 +0530

    [samarjeet]: added access and modify static fields

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/intermediatelevel/accessmodifystaticfields/Configuration.java
@@ -0,0 +1,7 @@
+package com.reflection.intermediatelevel.accessmodifystaticfields;
+public class Configuration {
+    private static String API_KEY = "DEFAULT_KEY";
+    public static void showKey() {
+        System.out.println("API_KEY " + API_KEY);
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/intermediatelevel/accessmodifystaticfields/ModifyStaticField.java
@@ -0,0 +1,28 @@
+package com.reflection.intermediatelevel.accessmodifystaticfields;
+
+import java.lang.reflect.Field;
+
+public class ModifyStaticField {
+    public static void main(String[] args) {
+        try {
+            // Get Class object
+            Class<?> cls = Configuration.class;
+            // Access private static field
+            Field field = cls.getDeclaredField("API_KEY");
+            // Allow access to private field
+            field.setAccessible(true);
+
+            //Get original value 
+            String originalKey = (String) field.get(null);
+            System.out.println("Original API_KEY" + originalKey);
+
+            //Modify value 
+            field.set(null, "NEW_SECRET_KEY");
+            //Get updated value
+            String updatedKey = (String) field.get(null);
+            System.out.println("Updated API_KEY" + updatedKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

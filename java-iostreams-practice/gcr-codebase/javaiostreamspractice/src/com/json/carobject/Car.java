009ebb844c102e27ba67b7a90739d146064e9fb0
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-01-31 13:25:50 +0530

    [samarjeet]:added java to json object program

---
+++ java-iostreams-practice/gcr-codebase/javaiostreamspractice/src/com/json/carobject/Car.java
@@ -0,0 +1,22 @@
+package com.json.carobject;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int price;
+    public Car(String brand, String model, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+    // Getters
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public int getPrice() {
+        return price;
+    }
+}

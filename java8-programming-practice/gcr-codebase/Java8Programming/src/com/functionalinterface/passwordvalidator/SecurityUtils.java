aa715e4696f03eb38cd955ab791bcff6b8af845e
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-02-02 14:58:03 +0530

    [samarjeet]: added password checker program

---
+++ java8-programming-practice/gcr-codebase/Java8Programming/src/com/functionalinterface/passwordvalidator/SecurityUtils.java
@@ -0,0 +1,14 @@
+package com.functionalinterface.passwordvalidator;
+
+	interface SecurityUtils {
+	    static boolean isStrongPassword(String password) {
+	        if (password == null) {
+	            return false;
+	        }
+	        boolean hasMinLength=password.length() >= 8;
+	        boolean hasUppercase=password.matches(".*[A-Z].*");
+	        boolean hasDigit=password.matches(".*\\d.*");
+	        return hasMinLength && hasUppercase && hasDigit;
+	    }
+	}
+

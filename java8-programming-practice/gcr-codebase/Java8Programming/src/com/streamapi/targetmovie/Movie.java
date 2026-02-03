f358b0126e75ac265e3158fcdd8bc5fb79179db1
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-02-03 11:57:25 +0530

    [samarjeet]: added target top movies stream program

---
+++ java8-programming-practice/gcr-codebase/Java8Programming/src/com/streamapi/targetmovie/Movie.java
@@ -0,0 +1,28 @@
+package com.streamapi.targetmovie;
+class Movie {
+    private String name;
+    private int releaseYear;
+    private double rating;
+   Movie(String name, int releaseYear, double rating) {
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + releaseYear + ") - Rating: " + rating;
+    }
+}

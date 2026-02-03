e594407d765d807a52b6afe49283fdff8a677d99
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-02-03 23:18:34 +0530

    [samarjeet]: added order revenue program

---
+++ java8-programming-practice/gcr-codebase/Java8Programming/src/com/collectors/orderrevenue/Order.java
@@ -0,0 +1,21 @@
+package com.collectors.orderrevenue;
+
+public class Order {
+private String customerName;
+private double amount;
+
+Order(String customerName,double amount){
+	this.customerName=customerName;
+	this.amount=amount;
+}
+public String getCustomerName() {
+	return customerName;
+}
+public double getAmount() {
+	return amount;
+}
+@Override
+public String toString() {
+	return customerName+ " "+ amount;
+}
+}

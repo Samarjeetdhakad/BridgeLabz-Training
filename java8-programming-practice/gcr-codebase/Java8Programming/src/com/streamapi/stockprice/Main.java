9d18ac4518b1709d5718ceadfdb17d8a8c5bf952
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-02-03 15:08:30 +0530

    [samarjeet]: added stock price program

---
+++ java8-programming-practice/gcr-codebase/Java8Programming/src/com/streamapi/stockprice/Main.java
@@ -0,0 +1,15 @@
+package com.streamapi.stockprice;
+import java.util.*;
+import java.util.stream.*;
+public class Main {
+	    public static void main(String[] args) {
+	        List<Stock> liveFeed = Arrays.asList(
+	            new Stock("CAPGE", 3890.50),
+	            new Stock("INFY", 1582.75),
+	            new Stock("RELIANCE", 2940.10),
+	            new Stock("HDFCBANK", 1645.30)
+	        );
+	        liveFeed.stream().forEach(stock -> System.out.println( "Stock: " + stock.getSymbol() + "  Price: " + stock.getPrice()));
+	    }
+	}
+

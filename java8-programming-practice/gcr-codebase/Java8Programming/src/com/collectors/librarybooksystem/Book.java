5f32b9c4e17aeb220d72325e75e6628be0f8bc06
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-02-04 00:24:20 +0530

    [samarjeet]: added library book system program

---
+++ java8-programming-practice/gcr-codebase/Java8Programming/src/com/collectors/librarybooksystem/Book.java
@@ -0,0 +1,19 @@
+package com.collectors.librarybooksystem;
+
+public class Book {
+private String genre;
+private int pages;
+Book(String genre,int pages){
+	this.genre=genre;
+	this.pages=pages;
+}
+public String getGenre() {
+	return genre;
+}
+public int getPages() {
+	return pages;
+}
+public String toString() {
+	return genre + " "+ pages;
+}
+}

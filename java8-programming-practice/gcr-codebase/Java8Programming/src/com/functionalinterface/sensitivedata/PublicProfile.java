8b78da8e021a8237b053f62f8ff8072d0e5b3911
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-02-03 11:58:30 +0530

    [samarjeet]: modify some files

--- java8-programming-practice/gcr-codebase/Java8Programming/src/com/functionalinterface/sensitivedata/PublicProfile.java
+++ java8-programming-practice/gcr-codebase/Java8Programming/src/com/functionalinterface/sensitivedata/PublicProfile.java
@@ -1,9 +1,7 @@
 package com.functionalinterface.sensitivedata;
 
 class PublicProfile {
-
     private String displayName;
-
     PublicProfile(String displayName) {
         this.displayName = displayName;
     }

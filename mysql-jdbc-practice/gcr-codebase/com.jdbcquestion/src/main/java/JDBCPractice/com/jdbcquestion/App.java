d0145f981018df448730238c0997230a236873d1
samarjeetdhakad <samarjeetdhakad4@gmail.com>
2026-02-16 16:04:58 +0530

    [samarjeet]: added jdbc student database program

---
+++ mysql-jdbc-practice/gcr-codebase/com.jdbcquestion/src/main/java/JDBCPractice/com/jdbcquestion/App.java
@@ -0,0 +1,10 @@
+package JDBCPractice.com.jdbcquestion;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
